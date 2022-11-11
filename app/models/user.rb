d81a7a3456497e3e804da513805a34a48cdfc1fd
Vinicius Vatte de Lima <vvatte@yahoo.com>
2022-11-10 22:14:23 -0300

    projeto

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :name, :address, :role, presence: true
+  validates :description, presence: true, if: :cooperative?
+
+  enum role: {
+    person: "person",
+    cooperative: "cooperative"
+  }
+
+  def cooperative?
+    role == "cooperative"
+  end
+end
